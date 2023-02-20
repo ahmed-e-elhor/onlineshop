174d53b7fb0e389497a484bb49fb44e7c95be06f
ahmed-e-elhor <ahmed.elhor@kortobaa.com>
2023-02-20 13:49:17 +0200

    updated sequence & application.ts

--- onlineshop/src/sequence.ts
+++ onlineshop/src/sequence.ts
@@ -1,3 +1,44 @@
-import {MiddlewareSequence} from '@loopback/rest';
+// import {MiddlewareSequence} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+// export class MySequence extends MiddlewareSequence {}
+
+import {AuthenticateFn, AuthenticationBindings} from '@loopback/authentication';
+import {inject} from '@loopback/context';
+import {FindRoute, InvokeMethod, InvokeMiddleware, ParseParams, Reject, RequestContext, RestBindings, Send, SequenceHandler} from '@loopback/rest';
+
+const SequenceActions = RestBindings.SequenceActions;
+
+export class MySequence implements SequenceHandler {
+  @inject(SequenceActions.INVOKE_MIDDLEWARE, {optional: true})
+  protected invokeMiddleware: InvokeMiddleware = () => false;
+
+  constructor(
+    @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
+    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
+    @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
+    @inject(SequenceActions.SEND) public send: Send,
+    @inject(SequenceActions.REJECT) public reject: Reject,
+    // inject AUTH_ACTION // ?? here
+    // in my app this wasnot here it useed middlerase-based Sequence
+    @inject(AuthenticationBindings.AUTH_ACTION) private authenticateRequest: AuthenticateFn,
+  ) {}
+
+  async handle(context: RequestContext) {
+    try {
+      const {request, response} = context;
+      const finished = await this.invokeMiddleware(context);
+      if (finished) return;
+      const route = this.findRoute(request);
+
+      //check our auth from request
+      await this.authenticateRequest(request);
+      const args = await this.parseParams(request, route);
+
+      //result of loopback requests
+      const result = await this.invoke(route, args);
+      this.send(response, result);
+    } catch (err) {
+      this.reject(context, err);
+    }
+  }
+}
